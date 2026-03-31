51fc4d7e6e1cac359e804d1627e8288a12877d0a
unknown <shubh386a@gmail.com>
2026-04-01 05:06:05 +0530

    chore: update backend configuration for vercel deployment and fix cloudinary variables

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import app from  "./app.js"
 import cloudinary from "cloudinary"
 
-cloudinary.v2.config({ // here we are configuring the cloudinary with the credentials which we get from cloudinary account
-    cloud_name:process.env.CLOUDINSRY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINSRY_API_SECRET
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
-const port = process.env.PORT || 3600;
-app.listen(port,()=>{
-    console.log(`Server listeing on ${port} port `)
-})
+
+const port = process.env.PORT || 4800;
+app.listen(port, () => {
+    console.log(`Server listening on ${port} port`);
+});
